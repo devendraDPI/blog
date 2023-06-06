1ee3f4a1d9144fe98d8088c9be5ce8251b45574c
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-06 13:39:02 +0530

    Posts by category

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from blog import views
+
+
+urlpatterns = [
+    path('<int:category_id>/', views.posts_by_category, name='posts-by-category'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from blog.models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status='published', category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except Exception:
+        return redirect('home')
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts-by-category.html', context)
