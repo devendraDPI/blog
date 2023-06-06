1a285a382c681da6efd4591f48d48a3600360b77
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-06 11:10:32 +0530

    Display featured posts on homepage

--- the_wise_wisdom/views.py
+++ the_wise_wisdom/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from blog.models import Category
+from blog.models import Category, Blog
 
 
 def home(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True).order_by('updated_at')
     context = {
         'categories': categories,
+        'featured_posts':featured_posts,
     }
     return render(request, 'home.html', context)
