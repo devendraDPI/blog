fd773e1ae692fecd7538b71888ff2af3f4f591d0
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-06 14:58:27 +0530

    Added context processor

---
+++ blog/context_processors.py
@@ -0,0 +1,6 @@
+from blog.models import Category
+
+
+def get_categories(request):
+    categories = Category.objects.all()
+    return {'categories': categories}
--- the_wise_wisdom/views.py
+++ the_wise_wisdom/views.py
@@ -3,13 +3,11 @@ from blog.models import Category, Blog
 
 
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(
         is_featured=True, status='published').order_by('updated_at')
     posts = Blog.objects.filter(
         is_featured=False, status='published')
     context = {
-        'categories': categories,
         'featured_posts': featured_posts,
         'posts': posts,
     }
