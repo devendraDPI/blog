91612b49952188167b68d8445ac69637c6b902b3
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-06 09:06:35 +0530

    Created Blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from blog.models import Category
+from blog.models import Category, Blog
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'status',
+                    'is_featured', 'created_at', 'updated_at')
+    search_fields = ('id', 'title', 'category__category_name',
+                    'author', 'status')
+    list_editable = ('status', 'is_featured',)
+    list_per_page = 10
+
+
 admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+BLOG_STATUS = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+)
+
+
 class Category(models.Model):
     category_name = models.CharField(max_length=128, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'categories'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.category_name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    short_description = models.TextField(max_length=1024)
+    blog_body = models.TextField(max_length=8192)
+    status = models.CharField(max_length=32, choices=BLOG_STATUS, default='draft')
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        return self.title
