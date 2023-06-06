d5c64fa0a700db9ecf5346b54a6f55827924f513
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-06 12:06:52 +0530

    Display recent articles on homepage

--- the_wise_wisdom/views.py
+++ the_wise_wisdom/views.py
@@ -4,9 +4,13 @@ from blog.models import Category, Blog
 
 def home(request):
     categories = Category.objects.all()
-    featured_posts = Blog.objects.filter(is_featured=True).order_by('updated_at')
+    featured_posts = Blog.objects.filter(
+        is_featured=True, status='published').order_by('updated_at')
+    posts = Blog.objects.filter(
+        is_featured=False, status='published')
     context = {
         'categories': categories,
-        'featured_posts':featured_posts,
+        'featured_posts': featured_posts,
+        'posts': posts,
     }
     return render(request, 'home.html', context)
