ad2bef632cfcc76884005f0aeaa0f5fa213f817d
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-06 08:21:04 +0530

    Configured media files

--- the_wise_wisdom/settings.py
+++ the_wise_wisdom/settings.py
@@ -126,6 +126,12 @@ STATICFILES_DIRS = [
 ]
 
 
+# media files (Images)
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
